8acee17287e6412c119950453089059e8c945391
felipe <felipe.lima@eq.ufcg.edu.br>
2019-03-10 22:23:08 -0300

    finished writing routine that reads simulation tree data file (Aspen Only)

---
+++ gui/callmainwindow.py
@@ -0,0 +1,44 @@
+import sys
+import pathlib
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QFileDialog
+from gui.views.mainwindow import *
+
+
+class MyForm(QMainWindow):
+
+    def __init__(self):
+        # initialization
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # internal variables initialization
+        self.sim_filename = ""
+
+        # signal/socket connections
+        self.ui.buttonOpenSimFile.clicked.connect(self.openSimFileDialog)
+
+    # open simulation file
+    def openSimFileDialog(self):
+        homedir = str(pathlib.Path.home())  # home directory (platform independent)
+        sim_filename, sim_filetype = QFileDialog.getOpenFileName(self, "Select .bkp simulation file", homedir,
+                                                                 "BKP files (*.bkp);; Text files (*.txt)")
+
+        if sim_filename == "" or sim_filetype != "BKP files (*.bkp)":
+            # user canceled the file dialog or selected invalid file
+            self.ui.textBrowserSimFile.setText("Invalid or no file selected.")
+            self.ui.textBrowserSimFile.setStyleSheet("color: red")
+
+        else:
+            # it's a valid file. Set its path as string and color. Also make its filepath available to the ui
+            self.sim_filename = sim_filename
+            self.ui.textBrowserSimFile.setText(sim_filename)
+            self.ui.textBrowserSimFile.setStyleSheet("color: blue")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MyForm()
+    w.show()
+
+    sys.exit(app.exec_())
---
+++ tests/gui/models/load_simtest.py
@@ -0,0 +1,19 @@
+import gui.models.load_simulation as lsim
+
+from gui.models.load_simulation import construct_tree_items
+
+stream_file = "/home/felipe/Desktop/GUI/python/AspenTreeStreams - Input _ Output.txt"
+
+
+def extract_name(node_string):
+    end_index = node_string.find(' =')  # search for equal symbol first
+
+    if end_index == -1:  # string has no equal
+        end_index = node_string.find(' --')
+
+    return node_string[:end_index].lstrip()  # the lstrip is to remove any leading whitespaces
+
+
+dt = lsim.read_simulation_tree(stream_file)
+
+input_node, output_node = construct_tree_items(dt)
