90b821d74954c7c0aad161adf0ae758822ac638a
felipe <felipe.lima@eq.ufcg.edu.br>
2019-03-11 21:52:34 -0300

    minor improvements in treeview. Still needs other functionalities.

--- tests/gui/views/simulation_tree.py
+++ tests/gui/views/simulation_tree.py
@@ -6,32 +6,46 @@ from gui.models.load_simulation import construct_tree_items, read_simulation_tre
 
 
 class MyForm(QDialog):
-    def __init__(self, streams_file_path, blocks_file_path):
-        super().__init__()  # initialize the QDialog
-        self.ui = Ui_Dialog()  # instantiate the Dialog Window
-        self.ui.setupUi(self)  # call the setupUi function to create and lay the window
+	def __init__(self, streams_file_path, blocks_file_path):
+		super().__init__()  # initialize the QDialog
+		self.ui = Ui_Dialog()  # instantiate the Dialog Window
+		self.ui.setupUi(self)  # call the setupUi function to create and lay the window
 
-        # create the model for the tree
-        model_tree_input = QStandardItemModel()
-        model_tree_ouput = QStandardItemModel()
-        self.ui.treeViewInput.setModel(model_tree_input)
-        self.ui.treeViewOutput.setModel(model_tree_ouput)
+		# create the model for the tree
+		model_tree_input = QStandardItemModel()
+		model_tree_output = QStandardItemModel()
 
-        # populate the data
+		model_tree_input.setHorizontalHeaderLabels(['Input variables'])
+		model_tree_output.setHorizontalHeaderLabels(['Output variables'])
 
-        # Load the stream tree
-        stream_raw = read_simulation_tree(streams_file_path)
-        stream_input, stream_output = construct_tree_items(stream_raw)
-        model_tree_input.appendRow(stream_input)
-        model_tree_ouput.appendRow(stream_output)
+		self.ui.treeViewInput.setModel(model_tree_input)
+		self.ui.treeViewOutput.setModel(model_tree_output)
+
+		# make the qtreeview headers bold
+		self.ui.treeViewInput.header().setStyleSheet('QWidget { font: bold }')
+		self.ui.treeViewOutput.header().setStyleSheet('QWidget { font: bold }')
+
+		# populate the data
+
+		# Load the stream tree
+		stream_raw = read_simulation_tree(streams_file_path)
+		stream_input, stream_output = construct_tree_items(stream_raw)
+		model_tree_input.appendRow(stream_input)
+		model_tree_output.appendRow(stream_output)
+
+		# load the blocks tree
+		blocks_raw = read_simulation_tree(blocks_file_path)
+		blocks_input, blocks_output = construct_tree_items(blocks_raw)
+		model_tree_input.appendRow(blocks_input)
+		model_tree_output.appendRow(blocks_output)
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+	app = QApplication(sys.argv)
 
-    stream_file = "/home/felipe/Desktop/GUI/python/AspenTreeStreams - Input _ Output.txt"
-    blocks_file = "/home/felipe/Desktop/GUI/python/AspenTreeBlocks - Input _ Output.txt"
-    w = MyForm(stream_file)
-    w.show()
+	stream_file = "/home/felipe/Desktop/GUI/python/AspenTreeStreams - Input _ Output.txt"
+	blocks_file = "/home/felipe/Desktop/GUI/python/AspenTreeBlocks - Input _ Output.txt"
+	w = MyForm(stream_file, blocks_file)
+	w.show()
 
-    sys.exit(app.exec_())
+	sys.exit(app.exec_())
