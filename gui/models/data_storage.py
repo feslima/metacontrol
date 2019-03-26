6b61e2913c0f022421992398cab54a8f17f415c8
Felipe <felipe.lima@eq.ufcg.edu.br>
2019-03-26 11:54:57 -0300

    data storage class implementation

---
+++ gui/models/data_storage.py
@@ -0,0 +1,42 @@
+from PyQt5.QtGui import QStandardItemModel
+
+
+class DataStorage(object):
+    """
+    Application data storage. This is for reuse of application data such as tree models, simulation data, aliases,
+    expressions, etc.
+    """
+    def __init__(self):
+        self._input_tree_model = None
+        self._output_tree_model = None
+        self._simulation_data = None
+
+    def getInputTreeModel(self):
+        return self._input_tree_model
+
+    def setInputTreeModel(self, tree_model):
+
+        if isinstance(tree_model, QStandardItemModel):
+            self._input_tree_model = tree_model
+        else:
+            raise TypeError("Input must be a QStandardItemModel class object.")
+
+    def getOutputTreeModel(self):
+        return self._output_tree_model
+
+    def setOutputTreeModel(self, tree_model):
+
+        if isinstance(tree_model, QStandardItemModel):
+            self._output_tree_model = tree_model
+        else:
+            raise TypeError("Input must be a QStandardItemModel class object.")
+
+    def getSimulationDataDictionary(self):
+        return self._simulation_data
+
+    def setSimulationDataDictionary(self, simulation_dictionary):
+
+        if isinstance(simulation_dictionary, dict):
+            self._simulation_data = simulation_dictionary
+        else:
+            raise TypeError("Input must be a dictionary object.")
