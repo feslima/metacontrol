11a9dcd558e72931e2bd3f99b703032a52ef4116
Felipe <felipe.lima@eq.ufcg.edu.br>
2019-04-25 17:19:47 -0300

    convergence flag selector for csv editor (initial commit)

---
+++ gui/calls/callconvergenceselector.py
@@ -0,0 +1,55 @@
+import csv
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtCore import Qt
+
+from gui.views.py_files.convergenceselector import Ui_Dialog
+
+
+class ConvergenceSelectorDialog(QDialog):
+    def __init__(self, header_list: list, status_index: int):
+        # ---------------------- dialog initialization ----------------------
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.setWindowFlags(Qt.Window)
+        self.setWindowModality(Qt.WindowModal)
+
+        # ------------------------------ WidgetInitialization ------------------------------
+        self.ui.comboBox.addItems(header_list)
+
+        # ------------------------------ Signals/Slots ------------------------------
+        self.ui.comboBox.currentIndexChanged.connect(self.indexChanged)
+
+        # ------------------------------ Internal Variables ------------------------------
+        self.status_index = status_index
+
+    def indexChanged(self):
+        self.status_index = self.ui.comboBox.currentIndex()
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+
+    header_list = ['case', 'status', 'rr', 'df', 'd', 'xb', 'b', 'qr', 'l', 'v', 'f', 'xd']
+    status_index = 0
+    w = ConvergenceSelectorDialog(header_list, status_index)
+    w.show()
+
+    if w.exec():
+        print(status_index)
+
+    def my_exception_hook(exctype, value, tback):
+        # Print the error and traceback
+        print(exctype, value, tback)
+        # Call the normal Exception hook after
+        sys.__excepthook__(exctype, value, tback)
+        sys.exit()
+
+    sys._excepthook = sys.excepthook
+
+    # Set the exception hook to our wrapping function
+    sys.excepthook = my_exception_hook
+
+    sys.exit(app.exec_())
---
+++ gui/views/py_files/convergenceselector.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_files\convergenceselector.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(370, 99)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
+        Dialog.setSizePolicy(sizePolicy)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
+        self.comboBox = QtWidgets.QComboBox(Dialog)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setObjectName("comboBox")
+        self.gridLayout.addWidget(self.comboBox, 1, 0, 1, 1)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Convergence flag selection"))
+        self.label.setText(_translate("Dialog", "Select the column name which corresponds to the CONVERGENCE FLAG:"))
+
