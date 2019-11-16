f3bc60f5abd61d1b7432533004ff606da7b358a2
Felipe <felipe.lima@eq.ufcg.edu.br>
2019-11-16 03:45:59 -0300

    rough implementation of the optimziation. UI still non responsive, needs move_to_thread

--- gui/calls/tabs/optimizationtab.py
+++ gui/calls/tabs/optimizationtab.py
@@ -1,7 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 
 from gui.models.data_storage import DataStorage
 from gui.views.py_files.caballerotab import Ui_Form
+
+from gui.models.sampling import CaballeroWorker, ReportObject
+from surropt.core.options.nlp import DockerNLPOptions
+
+from win32com.client import Dispatch
 
 
 class OptimizationTab(QWidget):
@@ -17,16 +22,79 @@ class OptimizationTab(QWidget):
 
         # ----------------------- Widget Initialization -----------------------
         # --------------------------- Signals/Slots ---------------------------
+        self.ui.startOptPushButton.clicked.connect(self.on_start_pressed)
+        self.ui.ipoptTestConnectionPushButton.clicked.connect(
+            self.on_ipopt_test_connection_pressed)
         # ---------------------------------------------------------------------
+
+    def on_start_pressed(self):
+        # get caballero parameters from ui
+        first_factor = float(self.ui.firstFactorLineEdit.text())
+        sec_factor = float(self.ui.secondFactorLineEdit.text())
+        tol_contract = float(self.ui.tolContractLineEdit.text())
+        con_tol = float(self.ui.conTolLineEdit.text())
+        penalty = float(self.ui.penaltyFactorLineEdit.text())
+        tol1 = float(self.ui.tol1LineEdit.text())
+        tol2 = float(self.ui.tol2LineEdit.text())
+        maxfunevals = int(self.ui.maxFunEvalsLineEdit.text())
+        regrpoly = self.ui.regrpolyComboBox.currentText()
+
+        params = {
+            'first_factor': first_factor,
+            'sec_factor': sec_factor,
+            'tol_contract': tol_contract,
+            'con_tol': con_tol,
+            'penalty': penalty,
+            'tol1': tol1,
+            'tol2': tol2,
+            'maxfunevals': maxfunevals,
+            'regrpoly': regrpoly,
+            'server_url': self.ui.ipoptAddressLineEdit.text()
+        }
+
+        # TODO: move worker to thread
+        # REMINDER: install ptvsd via pip to debug threads
+        # https://github.com/microsoft/ptvsd/issues/1189#issuecomment-468406399
+        ro = ReportObject(terminal=False)
+        ro.iteration_printed.connect(self.on_iteration_printed)
+
+        cw = CaballeroWorker(app_data=self.application_database, params=params,
+                             report=ro)
+
+        cw.start_optimization()
+
+        a = 1
+
+    def on_ipopt_test_connection_pressed(self):
+        # tests the connection with the server specified in the UI
+        srv_url = self.ui.ipoptAddressLineEdit.text()
+        try:
+            nlp_opts = DockerNLPOptions(name='test wsl', server_url=srv_url)
+        except ValueError as con_err:
+            # connection failed, inform the user
+            fail_box = QMessageBox(QMessageBox.Critical, 'Connection failed!',
+                                   str(con_err), buttons=QMessageBox.Ok,
+                                   parent=None)
+            fail_box.exec_()
+        else:
+            # connection succeed, let the user know it
+            suc_box = QMessageBox(QMessageBox.Information, 'Connection OK!',
+                                  'Connection to the ipopt was successful!',
+                                  buttons=QMessageBox.Ok, parent=None)
+            suc_box.exec_()
+
+    def on_iteration_printed(self, iter_msg: str):
+        self.ui.controlPanelTextBrowser.append(iter_msg)
 
 
 if __name__ == "__main__":
     import sys
     from gui.calls.base import my_exception_hook
+    from tests_.mock_data import DOE_TAB_MOCK_DS
 
     app = QApplication(sys.argv)
 
-    ds = DataStorage()
+    ds = DOE_TAB_MOCK_DS
     w = OptimizationTab(application_database=ds,
                         parent_tab=None)
     w.show()
--- gui/models/sampling.py
+++ gui/models/sampling.py
@@ -2,8 +2,13 @@ import numpy as np
 import pandas as pd
 import pythoncom
 import win32com.client
+from py_expression_eval import Parser
 from pydace.utils import lhsdesign
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+
+from surropt.caballero import Caballero, CaballeroOptions
+from surropt.core.options.nlp import DockerNLPOptions
+from surropt.core.procedures.output import Report
 
 from gui.models.data_storage import DataStorage
 from gui.models.sim_connections import AspenConnection
@@ -36,8 +41,7 @@ class SamplerThread(QThread):
         # https://stackoverflow.com/questions/26764978/using-win32com-with-multithreading
         # initialize
         pythoncom.CoInitialize()
-        self._aspen_connection = AspenConnection(
-            app_data.simulation_file)
+        self._aspen_connection = AspenConnection(app_data.simulation_file)
 
         # create id
         self._aspen_id = pythoncom.CoMarshalInterThreadInterfaceInStream(
@@ -120,3 +124,200 @@ def run_case(mv_values: list, output_data: list, aspen_obj):
             res_dict[out_var['var']] = np.spacing(1)
 
     return res_dict
+
+
+class SamplerWorker(QObject):
+
+    def __init__(self, app_data: DataStorage,
+                 marshalled_sim_id):
+        self.app_data = app_data
+        self.marshall_id = marshalled_sim_id
+        # alias to index mapping of variables for easy access
+        self.inp_aliases = {var['Alias']: idx for var, idx in
+                            enumerate(self.app_data.input_table_data)}
+
+    def sample_case(self, mv_values: dict):
+        """Samples a single case based on values provided in `mv_values`.
+
+        Parameters
+        ----------
+        mv_values : dict
+            Dictionary where the keys are the aliases of the input variables
+            and the values are the numeric values of each variable.
+        """
+
+        pythoncom.CoInitialize()
+        aspen_obj = win32com.client.Dispatch(
+            pythoncom.CoGetInterfaceAndReleaseStream(self.marshall_id,
+                                                     pythoncom.IID_IDispatch)
+        )
+
+        # set input values
+        for alias, value in mv_values.items():
+            if alias in self.inp_aliases:
+                # if the value exists in the input variables, set it in the
+                # simulation
+                row = self.app_data.input_table_data[self.inp_aliases[alias]]
+                aspen_obj.Tree.FindNode(row['Path']).Value = value
+
+        # get the output
+        res_dict = {}
+        UOSTAT2_val = aspen_obj.Tree.FindNode(
+            r"\Data\Results Summary\Run-Status\Output\UOSSTAT2").Value
+        if UOSTAT2_val == 8:
+            res_dict['success'] = True
+            for out_var in self.app_data.output_table_data:
+                res_dict[out_var['Alias']] = aspen_obj.Tree.FindNode(
+                    out_var['Path']).Value
+
+        else:
+            res_dict['success'] = False
+            for out_var in self.app_data.output_table_data:
+                res_dict[out_var['Alias']] = 1.0
+
+        pythoncom.CoUninitialize()  # unmarshal the thread
+        return res_dict
+
+
+class ReportObject(Report, QObject):
+    iteration_printed = pyqtSignal(str)
+
+    def __init__(self, terminal=False, plot=False):
+        super().__init__(terminal=terminal, plot=plot)
+
+    def print_iteration(self, iter_count, x, f_pred, f_actual, g_actual,
+                        header=False, color_font=None):
+        # capture message from Report class and send it to the gui
+        str_msg = super().print_iteration(iter_count, x, f_pred, f_actual,
+                                          g_actual, header=header,
+                                          color_font=color_font)
+        self.iteration_printed.emit(str_msg)
+        return str_msg
+
+    def get_results_report(self, index, r, x, f, lb, ub, fun_evals):
+        res_msg = super().get_results_report(index, r, x, f, lb, ub, fun_evals)
+        self.iteration_printed.emit(res_msg)
+        return res_msg
+
+
+class CaballeroWorker(QObject):
+
+    def __init__(self, app_data: DataStorage, params: dict,
+                 report: ReportObject, parent=None):
+        super().__init__(parent=parent)
+
+        self.app_data = app_data
+        self.params = params
+        self.parser = Parser()
+        self.report = report
+
+    def start_optimization(self):
+        params = self.params
+
+        # unzip parameter data
+        first_factor = params['first_factor']
+        sec_factor = params['sec_factor']
+        tol_contract = params['tol_contract']
+        con_tol = params['con_tol']
+        penalty = params['penalty']
+        tol1 = params['tol1']
+        tol2 = params['tol2']
+        maxfunevals = params['maxfunevals']
+        regrpoly = params['regrpoly']
+        server_url = params['server_url']
+
+        # setup the problem data
+        inp_aliases = [var['Alias'] for var in self.app_data.input_table_data
+                       if var['Type'] == 'Manipulated (MV)']
+        con_aliases = [var['Alias']
+                       for var in self.app_data.expression_table_data
+                       if var['Type'] == 'Constraint function']
+        obj_alias = [var['Alias']
+                     for var in self.app_data.expression_table_data
+                     if var['Type'] == 'Objective function (J)']
+
+        doe = pd.DataFrame(self.app_data.doe_sampled_data)
+        x = doe.loc[:, inp_aliases].to_numpy()
+        g = doe.loc[:, con_aliases].to_numpy()
+        f = doe.loc[:, obj_alias].to_numpy().flatten()
+
+        # open the aspen connection
+        self.open_connection()
+
+        # define model function
+        def model_fun(x): return self.model_function(x)
+
+        # nlp bounds
+        lb_list, ub_list = map(
+            list, zip(*[(row['lb'], row['ub'])
+                        for row in self.app_data.doe_mv_bounds])
+        )
+
+        # nlp options (assumes that the user already tested the connection)
+        nlp_opts = DockerNLPOptions(name='nlp-server',
+                                    server_url=server_url)
+
+        # algorithm options
+        cab_opts = CaballeroOptions(max_fun_evals=maxfunevals,
+                                    feasible_tol=con_tol,
+                                    penalty_factor=penalty,
+                                    ref_tol=tol1, term_tol=tol2,
+                                    first_factor=first_factor,
+                                    second_factor=sec_factor,
+                                    contraction_tol=tol_contract)
+
+        opt_obj = Caballero(x=x, g=g, f=f, model_function=model_fun,
+                            lb=lb_list, ub=ub_list, regression=regrpoly,
+                            options=cab_opts, nlp_options=nlp_opts,
+                            report_options=self.report)
+
+        opt_obj.optimize()
+
+        self.asp_obj.close_connection()
+
+    def open_connection(self):
+        pythoncom.CoInitialize()
+        asp_obj = AspenConnection(self.app_data.simulation_file)
+        self.asp_obj = asp_obj
+        self._aspen_connection = asp_obj.get_connection_object()
+
+    def model_function(self, x):
+        input_vars = [{'var': row['Alias'], 'Path': row['Path']}
+                      for row in self.app_data.input_table_data
+                      if row['Type'] == 'Manipulated (MV)']
+
+        output_vars = [{'var': row['Alias'], 'Path': row['Path']}
+                       for row in self.app_data.output_table_data]
+
+        # update input values
+        [var.update({'value': x[idx]}) for idx, var in enumerate(input_vars)]
+
+        # query the simulation engine, store the results
+        results = run_case(input_vars, output_vars, self._aspen_connection)
+
+        # evaluate constraint and objective functions
+        expr_values = {}
+        parser = self.parser
+        for expr in self.app_data.expression_table_data:
+            expr_to_parse = parser.parse(expr['Expr'])
+            var_list = expr_to_parse.variables()
+            expr_values[expr['Alias']] = expr_to_parse.evaluate(results)
+
+        # separate constraints values
+        g = [expr_values[row['Alias']]
+             for row in self.app_data.expression_table_data
+             if row['Type'] == 'Constraint function']
+
+        # objective function
+        f = [expr_values[row['Alias']]
+             for row in self.app_data.expression_table_data
+             if row['Type'] == 'Objective function (J)']
+
+        res = {
+            'status': results['success'] == 'ok',
+            'f': np.array(f).item(),
+            'g': g,
+            'extras': []
+        }
+
+        return res
