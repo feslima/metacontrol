bdb62782f3e14b97507ccfc9b1a12fbfcdb852d7
Felipe <felipe.lima@eq.ufcg.edu.br>
2019-11-21 23:23:50 -0300

    mock_data now loads DS mocks based on function call

--- tests_/mock_data.py
+++ tests_/mock_data.py
@@ -13,20 +13,32 @@ MTC_FOLDER_PATH = TESTS_FOLDER_PATH.joinpath('save_open')
 
 
 # --------------------------- DATASTORE FOR MOCKING ---------------------------
-# loasim and sampling(doe) tabs
-LOADSIM_SAMPLING_MOCK_DS = DataStorage()
-LOADSIM_SAMPLING_MOCK_DS.load(MTC_FOLDER_PATH / 'loadsimtab.mtc')
-LOADSIM_SAMPLING_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
+def loadsim_mock():
+    # loasim and sampling(doe) tabs
+    LOADSIM_SAMPLING_MOCK_DS = DataStorage()
+    LOADSIM_SAMPLING_MOCK_DS.load(MTC_FOLDER_PATH / 'loadsimtab.mtc')
+    LOADSIM_SAMPLING_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
 
-# lhs settings dialog
-_mtc_lhs_settings = {'n_samples': 50, 'n_iter': 10, 'inc_vertices': False}
-LHSSETTINGS_MOCK_DS = DataStorage()
-LHSSETTINGS_MOCK_DS.doe_lhs_settings = _mtc_lhs_settings
+    return LOADSIM_SAMPLING_MOCK_DS
 
-# doetab and sampling assistant
-DOE_TAB_MOCK_DS = DataStorage()
-DOE_TAB_MOCK_DS.load(MTC_FOLDER_PATH / 'doetab_sampling_assistant.mtc')
-DOE_TAB_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
+
+def lhssettings_mock():
+    # lhs settings dialog
+    _mtc_lhs_settings = {'n_samples': 50, 'n_iter': 10, 'inc_vertices': False}
+    LHSSETTINGS_MOCK_DS = DataStorage()
+    LHSSETTINGS_MOCK_DS.doe_lhs_settings = _mtc_lhs_settings
+
+    return LHSSETTINGS_MOCK_DS
+
+
+def doetab_mock():
+    # doetab and sampling assistant
+    DOE_TAB_MOCK_DS = DataStorage()
+    DOE_TAB_MOCK_DS.load(MTC_FOLDER_PATH / 'doetab_sampling_assistant.mtc')
+    DOE_TAB_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
+
+    return DOE_TAB_MOCK_DS
+
 
 # csveditor dialog
 CSVEDIT_PAIRINFO_MOCK = {'B': {'alias': 'b', 'status': True},
@@ -42,17 +54,29 @@ CSVEDIT_PAIRINFO_MOCK = {'B': {'alias': 'b', 'status': True},
                          'XB': {'alias': 'xb', 'status': True},
                          'XD': {'alias': 'xd', 'status': True}}
 
-# reduced space tab
-REDSPACE_TAB_MOCK_DS = DataStorage()
-REDSPACE_TAB_MOCK_DS.load(MTC_FOLDER_PATH / "reducedspacetab.mtc")
-REDSPACE_TAB_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
 
-# hessian extraction tab
-HESSIAN_TAB_MOCK_DS = DataStorage()
-HESSIAN_TAB_MOCK_DS.load(MTC_FOLDER_PATH / "hessianextractiontab.mtc")
-HESSIAN_TAB_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
+def redspace_mock():
+    # reduced space tab
+    REDSPACE_TAB_MOCK_DS = DataStorage()
+    REDSPACE_TAB_MOCK_DS.load(MTC_FOLDER_PATH / "reducedspacetab.mtc")
+    REDSPACE_TAB_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
 
-# soc tab
-SOC_TAB_MOC_DS = DataStorage()
-SOC_TAB_MOC_DS.load(MTC_FOLDER_PATH / "soctab.mtc")
-SOC_TAB_MOC_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
+    return REDSPACE_TAB_MOCK_DS
+
+
+def hessian_mock():
+    # hessian extraction tab
+    HESSIAN_TAB_MOCK_DS = DataStorage()
+    HESSIAN_TAB_MOCK_DS.load(MTC_FOLDER_PATH / "hessianextractiontab.mtc")
+    HESSIAN_TAB_MOCK_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
+
+    return HESSIAN_TAB_MOCK_DS
+
+
+def soc_mock():
+    # soc tab
+    SOC_TAB_MOC_DS = DataStorage()
+    SOC_TAB_MOC_DS.load(MTC_FOLDER_PATH / "soctab.mtc")
+    SOC_TAB_MOC_DS.simulation_file = str(ASPEN_BKP_FILE_PATH)
+
+    return SOC_TAB_MOC_DS
