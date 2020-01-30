3b0355b72e5643600f4ab483248edf8f52600059
Felipe <feslima93@gmail.com>
2020-01-30 14:59:43 -0300

    ui files recompilation

--- gui/views/py_files/doetab.py
+++ gui/views/py_files/doetab.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\Felipe\PycharmProjects\metacontrol\gui\views\ui_files\doetab.ui'
+# Form implementation generated from reading ui file 'C:\Users\Felipe\metacontrol\gui\views\ui_files\doetab.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
