6ebfaf54b7b84d6862af2c4eab1de13b45b2a669
Felipe <felipe.lima@eq.ufcg.edu.br>
2020-05-02 20:30:01 -0300

    reorganizing docs structure folders

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -48,13 +48,6 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'classic'
-html_theme_options = {
-    # "relbarbgcolor": "black",
-    # "sidebarbgcolor": "gray",
-    # "sidebarlinkcolor": "black",
-    # "sidebartextcolor": "black"
-    # "bgcolor": "black"
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
